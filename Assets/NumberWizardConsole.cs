92abb695b128a9d974eb1ca4693e6d0afb39e847
Yazzen El-Safadi <yazzenel-safadi@Yazzens-MacBook-Air.local>
2021-05-05 19:19:31 +0200

    Practice c# - first commit

---
+++ Assets/NumberWizardConsole.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizardConsole : MonoBehaviour
+{
+    //Class universal area so they can accessible to the functions blocks below
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //max being an integer
+
+
+        Debug.Log("Welcome to number wizard!");
+        Debug.Log("Pick a number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down ? Lower, Push Enter = Correct");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Works when if UpArrow key KeyCode has been pressed
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed. ");
+            min = guess;
+            Debug.Log(guess);
+
+        }//Works when if DownArrow key KeyCode has been pressed
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed. ");
+            max = guess;
+            Debug.Log(guess);
+        } //Works when if Return/Enter key KeyCode has been pressed
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key has been pressed");
+        }
+    }
+}
