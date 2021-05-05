5b49a86e943ca3571f6867b1f199f7e198dc0daa
Yazzen El-Safadi <yazzenel-safadi@Yazzens-MacBook-Air.local>
2021-05-05 19:48:29 +0200

    Adding (max +min)/2

--- Assets/NumberWizardConsole.cs
+++ Assets/NumberWizardConsole.cs
@@ -19,7 +19,7 @@ public class NumberWizardConsole : MonoBehaviour
         Debug.Log("Pick a number, don't tell me what it is...");
         Debug.Log("The highest number you can pick is: " + max);
         Debug.Log("The lowest number you can pick is: " + min);
-        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Tell me if your number is higher or lower than: " + guess);
         Debug.Log("Push Up = Higher, Push Down ? Lower, Push Enter = Correct");
 
     }
@@ -31,7 +31,11 @@ public class NumberWizardConsole : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed. ");
+            //if the guess is to low 
             min = guess;
+            //The it will add the following formula having a new value assigned
+            //to guess. This is the logic to updating the guess
+            guess = (max + min) / 2;
             Debug.Log(guess);
 
         }//Works when if DownArrow key KeyCode has been pressed
@@ -39,6 +43,7 @@ public class NumberWizardConsole : MonoBehaviour
         {
             Debug.Log("Down Arrow key was pressed. ");
             max = guess;
+            guess = (max + min) / 2;
             Debug.Log(guess);
         } //Works when if Return/Enter key KeyCode has been pressed
         else if (Input.GetKeyDown(KeyCode.Return))
